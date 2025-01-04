c1448805a3ce32d8e4805cb5f089fbcbfd2c7ec6
piklprado <prado@ib.usp.br>
2025-01-04 01:02:54 -0300

    seccion tutoriales index, tutoriales, slides clase 1

---
+++ docs/Tutoriales/create_swril_file.R
@@ -0,0 +1,10 @@
+nome <- commandArgs(trailingOnly = TRUE)
+library(swirlify)
+rota0 <- "Curso_R_UNMSM"
+rota <- paste0("tmp/Curso_R_UNMSM_v",nome)
+if(!dir.exists("tmp")) dir.create("tmp")
+file.copy(paste0(rota0,"/"), "tmp", overwrite = TRUE, recursive = TRUE)
+file.rename(paste0("tmp/",rota0), rota) 
+set_lesson(path_to_yaml = paste0(rota, "/Mi_primera_session/lesson.yaml"), open_lesson = FALSE)
+pack_course("~/work/pos_grad/usp/disciplinas/cursoR/UNMSM/2025/repo_material_didatico/Tutoriales/")
+system(paste0("rm -r ", rota))
