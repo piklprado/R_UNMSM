aaa5812abefb9f4187df34da0794c8f1b1e43581
piklprado <prado@ib.usp.br>
2025-01-04 21:10:07 -0300

    remove tutoriais swril, transferidos para https://github.com/piLaboratory/Curso_R_UNMSM

---
+++ Tutoriales/generate_tutorials_html.R
@@ -0,0 +1,31 @@
+## WIP: no funciona bien y solo en la maquina local
+library(swirlify)
+library(swirl)
+library(yaml)
+library(stringr)
+##source("lesson_to_rmd.R")
+
+any_of_exprs <- function(...){
+    lista <- list(...)
+    expr <- parse(text = lista[[1]])
+    eval(expr)
+    }
+
+manifest <- read.table("../Curso_R_UNMSM/MANIFEST")$V1
+ddir <- "/home/paulo/work/pos_grad/usp/disciplinas/cursoR/UNMSM/2025/repo_material_didatico/Tutoriales/html"
+
+
+
+## for(lesson in manifest){
+##     set_lesson(file.path("..", "Curso_R_UNMSM",lesson,"lesson.yaml"), open_lesson=FALSE, silent=TRUE)
+##     lesson_to_html(install_course=FALSE, dest_dir=ddir)
+## }
+
+f1 <- function(nome){
+     set_lesson(file.path("..", "Curso_R_UNMSM",nome,"lesson.yaml"), open_lesson=FALSE, silent=TRUE)
+     lesson_to_html(install_course=FALSE , dest_dir=ddir)
+     }
+## Por alguma razao da pau
+f1(manifest[1])
+## os excluidos dão pau
+lapply(manifest[c(-1,-6, -9, -10, -11)], f1)
